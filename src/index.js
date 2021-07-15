d7885bebe3ef8cfa8a8d6fedad50b776378f074e
Jefferson Candido <jefferson.ufu@gmail.com>
2021-07-15 00:57:07 -0300

    Creating user

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+
+  const { name, username } = request.body;
+  
+  const newUser = { 
+    id: uuidv4(),
+    name, 
+    username, 
+    todos: []
+  };
+
+  Object.assign(users, newUser);
+
+  return response.json(newUser);
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
